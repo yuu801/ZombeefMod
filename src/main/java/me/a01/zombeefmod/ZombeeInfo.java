1fd71a35724cf29b64d5441518e6780d43d04524
yuu <yuhei0801a@yahoo.co.jp>
2015-07-04 19:57:33 +0900

    Zombeef Mod source

---
+++ src/main/java/me/a01/zombeefmod/ZombeeInfo.java
@@ -0,0 +1,6 @@
+package me.a01.zombeefmod;
+
+public class ZombeeInfo {
+    public static final String MODID = "zombeefmod";
+    public static final String VERSION = "1.0";
+}
---
+++ src/main/java/me/a01/zombeefmod/ZombeeMain.java
@@ -0,0 +1,9 @@
+package me.a01.zombeefmod;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(modid = ZombeeMain.MODID, version = ZombeeMain.VERSION)
+public class ZombeeMain {
+    public static final String MODID = "zombeefmod";
+    public static final String VERSION = "1.0";
+}
